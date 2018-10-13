38db9e42183d79b370efe23035042d512999b002
Dominic Tarr <dominic.tarr@gmail.com>
2018-10-13 20:14:39 +1300

    implement interpreter

--- interpreter.js
+++ interpreter.js
@@ -1,6 +1,6 @@
 
 function interpreter (value, ast, stack) {
-  if(!exports[ast.name]) throw new Error('unknown method:'+ast.name)
+  if(!exports[ast.name]) throw new Error('unknown method:'+ast.name+', '+JSON.stringify(ast))
   if(!stack) stack = [value]
   return exports[ast.name].call(null, value, ast.value, stack || [])
 }
@@ -15,10 +15,14 @@ exports.pipe = function (value, ops, stack) {
   return value
 }
 
-exports.get = function (value, key) {
-  return value[key]
+//only allows exact strings?
+exports.get = function (value, path) {
+  console.log("GET", value, path)
+  return value[path[0]]
 }
 
+
+//special for async
 exports.object = function (value, pairs, stack) {
   var o = {}
 
@@ -30,8 +34,9 @@ exports.object = function (value, pairs, stack) {
   return o
 }
 
-exports.parent = function (value, parent, stack) {
-  return stack[parent]
+//special for async
+exports.parent = function (value, args, stack) {
+  return stack[args[0]]
 }
 
 function interpret_arg (value, arg, stack) {
@@ -41,37 +46,61 @@ function interpret_arg (value, arg, stack) {
     return arg
 }
 
-exports.filter = function (value, arg, stack) {
-  return value.filter(function (e) {
-    var v = interpret_arg(e, arg, [e].concat(stack))
+function filter(array, each) {
+  return array.filter(each)
+}
 
-    return v
+//special for async
+exports.filter = function (value, args, stack) {
+  return filter(value, function (e) {
+    return interpret_arg(e, args[0], [e].concat(stack))
   })
 }
 
-exports.map = function (value, arg, stack) {
+//special for async
+exports.map = function (value, args, stack) {
   return value.map(function (e) {
-    var v = interpret_arg(e, arg, [e].concat(stack))
-
-    return v
+    return interpret_arg(e, args[0], [e].concat(stack))
   })
 }
 
-exports.if = function (value, arg, stack) {
+//special handling for async
+exports.if = function (value, args, stack) {
   return (
-      interpret_arg(value, arg[0], stack)
-    ? interpret_arg(value, arg[1], stack)
-    : interpret_arg(value, arg[2], stack)
+      interpret_arg(value, args[0], stack)
+    ? interpret_arg(value, args[1], stack)
+    : interpret_arg(value, args[2], stack)
   )
 }
 
 //basic operators
-exports.gt = function (value, arg) {
-  return value > arg
+//I think these can have the arg pre-evaluated
+
+var basic = require('./basic')
+
+for(var k in basic) (function (fn, name) {
+  exports[name] = function (value, args, stack) {
+    return fn(value, interpret_arg(value, args[0], stack))
+  }
+})(basic[k], k)
+
+exports.reduce = function (value, args, stack) {
+  var initial = interpret_arg(value, args[1], stack)
+  return value.reduce(function (a, b) {
+    return interpret_arg(a, args[0], [a, b].concat(stack))
+  }, initial)
 }
 
-exports.eq = function (value, arg, stack) {
-  return value === interpret_arg(value, arg, stack)
+exports.set = function (value, args, stack) {
+  if(value == null) value = {}
+  var key = interpret_arg(value, args[0], stack)
+  value[key] = interpret_arg(value[key], args[1], stack)
+  return value
 }
+
+
+
+
+
 
 
